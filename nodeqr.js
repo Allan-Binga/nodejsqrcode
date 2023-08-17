c92f7fe6f3574e004d422b782da60a91fece31b7
Allan-Binga <allanbinga73@gmail.com>
2023-08-16 20:49:07 -0400

    Initial

---
+++ nodeqr.js
@@ -0,0 +1,11 @@
+const QRCode = require('qrcode')
+
+const generateQR = async text => {
+    try {
+        console.log(await QRCode.toDataURL(text))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+generateQR("http://yahoo.com/")
